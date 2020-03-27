4f059ebb2d3080affde548d85e1a7376d6861266
Iresharma <iresh.sharma8@gmail.com>
2020-03-27 15:02:59 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+void main() {
+    runApp()
+}
+
+class umm extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home: Scaffold(
+                appBar: AppBar(
+                    title: Text('App'),
+                ),
+            ),
+        ),
+    }
+}
